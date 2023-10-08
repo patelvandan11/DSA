aa92ef1bda0b1647a2d115b5fa326aebfff4ef2b
patel vandan <137554503+ptlVandan@users.noreply.github.com>
2023-10-08 20:18:38 +0530

    update

---
+++ c language/DSA COLLEGE/1.C.Write a program to read 10 integers in an array. Sort them out on the basis of number of  digits in each element.C
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+int main()
+{
+
+    int v[5];
+    printf("ente the element :");
+    for (int i = 0; i < 5; i++)
+    {
+        scanf("%d", &v[i]);
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        int temp = 0;
+        if (v[i] > v[i + 1])
+        {
+            temp = v[i];
+            v[i] = v[i + 1];
+            v[i + 1] = temp;
+        }
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d", v[i]);
+    }
+
+    return 0;
+}
