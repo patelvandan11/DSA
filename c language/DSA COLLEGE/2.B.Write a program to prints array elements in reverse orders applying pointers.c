9147e61a488ee9a7beb01185fe0cd0d0fe7a2b01
patel vandan <137554503+ptlVandan@users.noreply.github.com>
2023-10-10 11:45:09 +0530

    update

---
+++ c language/DSA COLLEGE/2.B.Write a program to prints array elements in reverse orders applying pointers.c
@@ -0,0 +1,24 @@
+// ●Write a program to prints array elements in reverse orders applying pointers
+
+#include <stdio.h>
+
+void reverse(int *arr, int size)
+{
+    int *ptr = arr + size - 1;
+    while (ptr >= arr)
+    {
+        printf("%d ", *ptr);
+        ptr--;
+    }
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    // printf("size :%d ", sizeof(arr[0]));
+    printf("\n");
+
+    reverse(arr, size);
+    return 0;
+}
